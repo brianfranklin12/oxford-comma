10d024a4438d8a755d2e91c1ae97f43ee61e2c58
Brian Franklin <brianfranklinmusic@gmail.com>
2020-09-29 14:21:40 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+case array.length
+when 1
+    array.join
+when 2
+    array.join(" and ")
+else
+    last_array = ", and #{array.pop()}"
+    string = array.join(", ")
+    
+    string << last_array
+end
 end
